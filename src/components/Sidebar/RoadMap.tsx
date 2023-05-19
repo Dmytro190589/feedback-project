386eb7dc811f2f4fd0be78ad05bc2d7273766e94
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-05-19 10:23:21 +0300

    123

--- src/components/Sidebar/RoadMap.tsx
+++ src/components/Sidebar/RoadMap.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 const Li = styled.li`
   display: flex;
-  list-style-type: circle;
   justify-content: space-between;
 `;
 
