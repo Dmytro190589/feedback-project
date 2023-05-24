f16d5b1a9a572417b9ffa111f2e71b684c6b2336
VladislavSurkov <fvhabjrega@gmail.com>
2023-05-24 16:54:35 +0300

    nav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,29 @@
-import { FC } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { Suggestions } from '../pages/Suggestions/Suggestions';
 import { SharedLayout } from './SharedLayout/SharedLayout';
+
 const App: FC = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/suggestions');
+    }
+  });
+
   return (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route path="/all" element={<div>all!!!</div>} />
-          <Route path="/ui" element={<div>ui!!!</div>} />
-          <Route path="/ux" element={<div>ux!!!</div>} />
-          <Route path="/enhancement" element={<div>en!!!</div>} />
-          <Route path="/bug" element={<div>bug!!!</div>} />
-          <Route path="/feature" element={<div>feature!!!</div>} />
-          <Route path="/suggestions" element={<Suggestions />}></Route>
+          <Route path="all" element={<div>all!!!</div>} />
+          <Route path="ui" element={<div>ui!!!</div>} />
+          <Route path="ux" element={<div>ux!!!</div>} />
+          <Route path="enhancement" element={<div>en!!!</div>} />
+          <Route path="bug" element={<div>bug!!!</div>} />
+          <Route path="feature" element={<div>feature!!!</div>} />
+          <Route path="suggestions" element={<Suggestions />}></Route>
         </Route>
       </Routes>
     </>
