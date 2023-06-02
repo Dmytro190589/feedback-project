56650b0e61f79a73e082b949dd8f12b7598c4100
VladislavSurkov <fvhabjrega@gmail.com>
2023-06-02 18:37:19 +0300

    select

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,11 +1,10 @@
-import Select, { OnChangeValue   } from 'react-select';
+import Select, { OnChangeValue} from 'react-select';
 import { useState, FC } from 'react';
 import { IOption } from 'models/CategoriesTypes';
-import { IndicatorSeparator } from './Dropdown.styled';
+import { IndicatorSeparator, colourStyles, themeStyles } from './Dropdown.styled';
 
 interface CurrentProps {
   prop: IOption[];
-  // component:any
 }
 
 export const Dropdown: FC<CurrentProps> = ({ prop }) => {
@@ -15,38 +14,12 @@ export const Dropdown: FC<CurrentProps> = ({ prop }) => {
 
   const onChange = (newValue: OnChangeValue<IOption, boolean>) =>
     setCurrentProp((newValue as IOption).value);
-
+  
   return (
     <>
       <Select
-        styles={{
-          control: () => ({
-            display: 'flex',
-          }),
-          singleValue: (base, state) => ({
-            ...base,
-            color: state ? '#f2f4febf' : '#f2f4fe',
-          }),
-          option: (base, { isFocused }) => ({
-            ...base,
-            color: isFocused ? '#AD1FEA' : '#647196',
-            ':active': {
-              color: '#AD1FEA',
-            },
-            ':not(:last-child)': {
-              borderBottom: '1px solid #3a43747f',
-            },
-          }),
-        }}
-        theme={theme => ({
-          ...theme,
-
-          colors: {
-            ...theme.colors,
-            primary25: 'none',
-            primary: 'none',
-          },
-        })}
+        styles={{ ...colourStyles }}
+        theme={themeStyles}
         components={{ IndicatorSeparator }}
         classNamePrefix="react-select"
         onChange={onChange}
